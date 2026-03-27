a6a922e76c36c91b4bbcafcc85e966b0b0cc2775
Altamish Mahomed <gambit318@gmail.com>
2026-03-27 11:24:24 +0200

    Buzzer integration.

--- trigger/trigger.ino
+++ trigger/trigger.ino
@@ -2,10 +2,11 @@ const int LED_PIN = 2;
 const int BUTTON_PIN = 3;
 const int BUZZER_PIN = 5;
 
-const int BUZZER_DURATION = 200;
-const int BUZZER_TONE = 3000;
+const int BUZZER_TONE_CLICK = 3000;
 
-const int TIMEOUT = 1500;
+const int BUZZER_DURATION = 200;
+
+const int TIMEOUT = 800;
 
 void setup() 
 {
@@ -22,7 +23,7 @@ void loop()
   if (pressed) 
   {
     digitalWrite(LED_PIN, HIGH);
-    tone(BUZZER_PIN, BUZZER_TONE, BUZZER_DURATION);
+    tone(BUZZER_PIN, BUZZER_TONE_CLICK, BUZZER_DURATION);
     delay(TIMEOUT);
   }
   else 
